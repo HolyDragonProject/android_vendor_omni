a521591cb4f1bd1428ee8f1f132443655783deb9
Miguel Martinez <slverwolf@gmail.com>
2018-04-25 10:50:27 -0500

    config/common: Enable new sounds
    Change-Id: Ie0e4e43915e80761ce50f6ae4e0fb49b0ef91087

--- config/common.mk
+++ config/common.mk
@@ -69,20 +69,139 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.config.notification_sound=omni_notification1.ogg \
     ro.config.alarm_alert=omni_alarm1.ogg
 
+# Omni Sounds
 PRODUCT_COPY_FILES += \
-    vendor/omni/prebuilt/sounds/camera_click_48k.ogg:system/media/audio/ui/camera_click.ogg \
-    vendor/omni/prebuilt/sounds/VideoRecord_48k.ogg:system/media/audio/ui/VideoRecord.ogg \
-    vendor/omni/prebuilt/sounds/VideoStop_48k.ogg:system/media/audio/ui/VideoStop.ogg \
-    vendor/omni/prebuilt/sounds/omni_ringtone1.ogg:system/media/audio/ringtones/omni_ringtone1.ogg \
-    vendor/omni/prebuilt/sounds/omni_ringtone2.ogg:system/media/audio/ringtones/omni_ringtone2.ogg \
-    vendor/omni/prebuilt/sounds/omni_ringtone3.ogg:system/media/audio/ringtones/omni_ringtone3.ogg \
-    vendor/omni/prebuilt/sounds/omni_alarm1.ogg:system/media/audio/alarms/omni_alarm1.ogg \
-    vendor/omni/prebuilt/sounds/omni_alarm2.ogg:system/media/audio/alarms/omni_alarm2.ogg \
-    vendor/omni/prebuilt/sounds/omni_notification1.ogg:system/media/audio/notifications/omni_notification1.ogg \
-    vendor/omni/prebuilt/sounds/omni_lowbattery1.ogg:system/media/audio/ui/omni_lowbattery1.ogg \
-    vendor/omni/prebuilt/sounds/omni_lock_phone.ogg:system/media/audio/ui/omni_lock_phone.ogg \
-    vendor/omni/prebuilt/sounds/omni_unlock_phone.ogg:system/media/audio/ui/omni_unlock_phone.ogg \
-    vendor/omni/prebuilt/sounds/omni_charge_start.ogg:system/media/audio/ui/omni_charge_start.ogg
+    vendor/omni/prebuilt/sounds/omni/omni_ringtone1.ogg:system/media/audio/ringtones/omni_ringtone1.ogg \
+    vendor/omni/prebuilt/sounds/omni/omni_ringtone2.ogg:system/media/audio/ringtones/omni_ringtone2.ogg \
+    vendor/omni/prebuilt/sounds/omni/omni_ringtone3.ogg:system/media/audio/ringtones/omni_ringtone3.ogg \
+    vendor/omni/prebuilt/sounds/omni/omni_alarm1.ogg:system/media/audio/alarms/omni_alarm1.ogg \
+    vendor/omni/prebuilt/sounds/omni/omni_alarm2.ogg:system/media/audio/alarms/omni_alarm2.ogg \
+    vendor/omni/prebuilt/sounds/omni/omni_notification1.ogg:system/media/audio/notifications/omni_notification1.ogg \
+    vendor/omni/prebuilt/sounds/omni/omni_lowbattery1.ogg:system/media/audio/ui/omni_lowbattery1.ogg \
+    vendor/omni/prebuilt/sounds/omni/omni_lock_phone.ogg:system/media/audio/ui/omni_lock_phone.ogg \
+    vendor/omni/prebuilt/sounds/omni/omni_unlock_phone.ogg:system/media/audio/ui/omni_unlock_phone.ogg \
+    vendor/omni/prebuilt/sounds/omni/omni_charge_start.ogg:system/media/audio/ui/omni_charge_start.ogg
+
+# Pixel2 Alarm Sounds
+PRODUCT_COPY_FILES += \
+    vendor/omni/prebuilt/sounds/google/alarms/A_real_hoot.ogg:system/media/audio/alarms/A_real_hoot.ogg \
+    vendor/omni/prebuilt/sounds/google/alarms/Bright_morning.ogg:system/media/audio/alarms/Bright_morning.ogg \
+    vendor/omni/prebuilt/sounds/google/alarms/Cuckoo_clock.ogg:system/media/audio/alarms/Cuckoo_clock.ogg \
+    vendor/omni/prebuilt/sounds/google/alarms/Early_twilight.ogg:system/media/audio/alarms/Early_twilight.ogg \
+    vendor/omni/prebuilt/sounds/google/alarms/Full_of_wonder.ogg:system/media/audio/alarms/Full_of_wonder.ogg \
+    vendor/omni/prebuilt/sounds/google/alarms/Gentle_breeze.ogg:system/media/audio/alarms/Gentle_breeze.ogg \
+    vendor/omni/prebuilt/sounds/google/alarms/Icicles.ogg:system/media/audio/alarms/Icicles.ogg \
+    vendor/omni/prebuilt/sounds/google/alarms/Jump_start.ogg:system/media/audio/alarms/Jump_start.ogg \
+    vendor/omni/prebuilt/sounds/google/alarms/Loose_change.ogg:system/media/audio/alarms/Loose_change.ogg \
+    vendor/omni/prebuilt/sounds/google/alarms/Rolling_fog.ogg:system/media/audio/alarms/Rolling_fog.ogg \
+    vendor/omni/prebuilt/sounds/google/alarms/Spokes.ogg:system/media/audio/alarms/Spokes.ogg \
+    vendor/omni/prebuilt/sounds/google/alarms/Sunshower.ogg:system/media/audio/alarms/Sunshower.ogg
+
+# Pixel2 Notification Sounds
+PRODUCT_COPY_FILES += \
+    vendor/omni/prebuilt/sounds/google/notifications/Beginning.ogg:system/media/audio/notifications/Beginning.ogg \
+    vendor/omni/prebuilt/sounds/google/notifications/Coconuts.ogg:system/media/audio/notifications/Coconuts.ogg \
+    vendor/omni/prebuilt/sounds/google/notifications/Duet.ogg:system/media/audio/notifications/Duet.ogg \
+    vendor/omni/prebuilt/sounds/google/notifications/End_note.ogg:system/media/audio/notifications/End_note.ogg \
+    vendor/omni/prebuilt/sounds/google/notifications/Gentle_gong.ogg:system/media/audio/notifications/Gentle_gong.ogg \
+    vendor/omni/prebuilt/sounds/google/notifications/Mallet.ogg:system/media/audio/notifications/Mallet.ogg \
+    vendor/omni/prebuilt/sounds/google/notifications/Orders_up.ogg:system/media/audio/notifications/Orders_up.ogg \
+    vendor/omni/prebuilt/sounds/google/notifications/Ping.ogg:system/media/audio/notifications/Ping.ogg \
+    vendor/omni/prebuilt/sounds/google/notifications/Pipes.ogg:system/media/audio/notifications/Pipes.ogg \
+    vendor/omni/prebuilt/sounds/google/notifications/Popcorn.ogg:system/media/audio/notifications/Popcorn.ogg \
+    vendor/omni/prebuilt/sounds/google/notifications/Shopkeeper.ogg:system/media/audio/notifications/Shopkeeper.ogg \
+    vendor/omni/prebuilt/sounds/google/notifications/Sticks_and_stones.ogg:system/media/audio/notifications/Sticks_and_stones.ogg \
+    vendor/omni/prebuilt/sounds/google/notifications/Tuneup.ogg:system/media/audio/notifications/Tuneup.ogg \
+    vendor/omni/prebuilt/sounds/google/notifications/Tweeter.ogg:system/media/audio/notifications/Tweeter.ogg \
+    vendor/omni/prebuilt/sounds/google/notifications/Twinkle.ogg:system/media/audio/notifications/Twinkle.ogg
+
+# Pixel2 Ringtone Sounds
+PRODUCT_COPY_FILES += \
+    vendor/omni/prebuilt/sounds/google/ringtones/Copycat.ogg:system/media/audio/ringtones/Copycat.ogg \
+    vendor/omni/prebuilt/sounds/google/ringtones/Crackle.ogg:system/media/audio/ringtones/Crackle.ogg \
+    vendor/omni/prebuilt/sounds/google/ringtones/Flutterby.ogg:system/media/audio/ringtones/Flutterby.ogg \
+    vendor/omni/prebuilt/sounds/google/ringtones/Hotline.ogg:system/media/audio/ringtones/Hotline.ogg \
+    vendor/omni/prebuilt/sounds/google/ringtones/Leaps_and_bounds.ogg:system/media/audio/ringtones/Leaps_and_bounds.ogg \
+    vendor/omni/prebuilt/sounds/google/ringtones/Lollipop.ogg:system/media/audio/ringtones/Lollipop.ogg \
+    vendor/omni/prebuilt/sounds/google/ringtones/Lost_and_found.ogg:system/media/audio/ringtones/Lost_and_found.ogg \
+    vendor/omni/prebuilt/sounds/google/ringtones/Mash_up.ogg:system/media/audio/ringtones/Mash_up.ogg \
+    vendor/omni/prebuilt/sounds/google/ringtones/Monkey_around.ogg:system/media/audio/ringtones/Monkey_around.ogg \
+    vendor/omni/prebuilt/sounds/google/ringtones/Schools_out.ogg:system/media/audio/ringtones/Schools_out.ogg \
+    vendor/omni/prebuilt/sounds/google/ringtones/The_big_adventure.ogg:system/media/audio/ringtones/The_big_adventure.ogg \
+    vendor/omni/prebuilt/sounds/google/ringtones/Zen_too.ogg:system/media/audio/ringtones/Zen_too.ogg
+
+# Pixel2 UI Sounds
+PRODUCT_COPY_FILES += \
+    vendor/omni/prebuilt/sounds/google/ui/audio_end.ogg:system/media/audio/ui/audio_end.ogg \
+    vendor/omni/prebuilt/sounds/google/ui/audio_initiate.ogg:system/media/audio/ui/audio_initiate.ogg \
+    vendor/omni/prebuilt/sounds/google/ui/camera_click.ogg:system/media/audio/ui/camera_click.ogg \
+    vendor/omni/prebuilt/sounds/google/ui/camera_focus.ogg:system/media/audio/ui/camera_focus.ogg \
+    vendor/omni/prebuilt/sounds/google/ui/Dock.ogg:system/media/audio/ui/Dock.ogg \
+    vendor/omni/prebuilt/sounds/google/ui/Effect_Tick.ogg:system/media/audio/ui/Effect_Tick.ogg \
+    vendor/omni/prebuilt/sounds/google/ui/KeypressDelete.ogg:system/media/audio/ui/KeypressDelete.ogg \
+    vendor/omni/prebuilt/sounds/google/ui/KeypressInvalid.ogg:system/media/audio/ui/KeypressInvalid.ogg \
+    vendor/omni/prebuilt/sounds/google/ui/KeypressReturn.ogg:system/media/audio/ui/KeypressReturn.ogg \
+    vendor/omni/prebuilt/sounds/google/ui/KeypressSpacebar.ogg:system/media/audio/ui/KeypressSpacebar.ogg \
+    vendor/omni/prebuilt/sounds/google/ui/KeypressStandard.ogg:system/media/audio/ui/KeypressStandard.ogg \
+    vendor/omni/prebuilt/sounds/google/ui/Lock.ogg:system/media/audio/ui/Lock.ogg \
+    vendor/omni/prebuilt/sounds/google/ui/LowBattery.ogg:system/media/audio/ui/LowBattery.ogg \
+    vendor/omni/prebuilt/sounds/google/ui/NFCFailure.ogg:system/media/audio/ui/NFCFailure.ogg \
+    vendor/omni/prebuilt/sounds/google/ui/NFCInitiated.ogg:system/media/audio/ui/NFCInitiated.ogg \
+    vendor/omni/prebuilt/sounds/google/ui/NFCSuccess.ogg:system/media/audio/ui/NFCSuccess.ogg \
+    vendor/omni/prebuilt/sounds/google/ui/NFCTransferComplete.ogg:system/media/audio/ui/NFCTransferComplete.ogg \
+    vendor/omni/prebuilt/sounds/google/ui/NFCTransferInitiated.ogg:system/media/audio/ui/NFCTransferInitiated.ogg \
+    vendor/omni/prebuilt/sounds/google/ui/Trusted.ogg:system/media/audio/ui/Trusted.ogg \
+    vendor/omni/prebuilt/sounds/google/ui/Undock.ogg:system/media/audio/ui/Undock.ogg \
+    vendor/omni/prebuilt/sounds/google/ui/Unlock.ogg:system/media/audio/ui/Unlock.ogg \
+    vendor/omni/prebuilt/sounds/google/ui/VideoRecord.ogg:system/media/audio/ui/VideoRecord.ogg \
+    vendor/omni/prebuilt/sounds/google/ui/VideoStop.ogg:system/media/audio/ui/VideoStop.ogg \
+    vendor/omni/prebuilt/sounds/google/ui/WirelessChargingStarted.ogg:system/media/audio/ui/WirelessChargingStarted.ogg 
+
+# Razer UI Sounds
+PRODUCT_COPY_FILES += \
+    vendor/omni/prebuilt/sounds/razer/ui/Lock.ogg:system/media/audio/ui/Lock.ogg \
+    vendor/omni/prebuilt/sounds/razer/ui/Unlock.ogg:system/media/audio/ui/Unlock.ogg
+
+# Extra Alarms
+PRODUCT_COPY_FILES += \
+    vendor/omni/prebuilt/sounds/extra/alarms/AAAAAAAAAAAAAAAGH!!!!.ogg:system/media/audio/alarms/AAAAAAAAAAAAAAAGH!!!!.ogg \
+    vendor/omni/prebuilt/sounds/extra/alarms/ArmyWakeUp.ogg:system/media/audio/alarms/ArmyWakeUp.ogg \
+    vendor/omni/prebuilt/sounds/extra/alarms/CyanAlarm.ogg:system/media/audio/alarms/CyanAlarm.ogg \
+    vendor/omni/prebuilt/sounds/extra/alarms/DawnChorus.ogg:system/media/audio/alarms/DawnChorus.ogg \
+    vendor/omni/prebuilt/sounds/extra/alarms/Daybreak.ogg:system/media/audio/alarms/Daybreak.ogg \
+    vendor/omni/prebuilt/sounds/extra/alarms/DrAlarm.ogg:system/media/audio/alarms/DrAlarm.ogg \
+    vendor/omni/prebuilt/sounds/extra/alarms/Feverfew.flac:system/media/audio/alarms/Feverfew.flac \
+    vendor/omni/prebuilt/sounds/extra/alarms/Foxglove.flac:system/media/audio/alarms/Foxglove.flac \
+    vendor/omni/prebuilt/sounds/extra/alarms/KHAlarm.ogg:system/media/audio/alarms/KHAlarm.ogg \
+    vendor/omni/prebuilt/sounds/extra/alarms/MAKEITSTAHP.ogg:system/media/audio/alarms/MAKEITSTAHP.ogg \
+    vendor/omni/prebuilt/sounds/extra/alarms/MusicBox.ogg:system/media/audio/alarms/MusicBox.ogg \
+    vendor/omni/prebuilt/sounds/extra/alarms/NewDay.ogg:system/media/audio/alarms/NewDay.ogg \
+    vendor/omni/prebuilt/sounds/extra/alarms/NuclearLaunch.ogg:system/media/audio/alarms/NuclearLaunch.ogg \
+    vendor/omni/prebuilt/sounds/extra/alarms/ResidentAlarm.ogg:system/media/audio/alarms/ResidentAlarm.ogg \
+    vendor/omni/prebuilt/sounds/extra/alarms/ResidentEvil.ogg:system/media/audio/alarms/ResidentEvil.ogg \
+    vendor/omni/prebuilt/sounds/extra/alarms/Scream.ogg:system/media/audio/alarms/Scream.ogg \
+    vendor/omni/prebuilt/sounds/extra/alarms/Serenity.ogg:system/media/audio/alarms/Serenity.ogg \
+    vendor/omni/prebuilt/sounds/extra/alarms/Surreal.ogg:system/media/audio/alarms/Surreal.ogg \
+    vendor/omni/prebuilt/sounds/extra/alarms/The_secret_forest.ogg:system/media/audio/alarms/The_secret_forest.ogg \
+    vendor/omni/prebuilt/sounds/extra/alarms/Timer.ogg:system/media/audio/alarms/Timer.ogg \
+    vendor/omni/prebuilt/sounds/extra/alarms/Valkyries.ogg:system/media/audio/alarms/Valkyries.ogg \
+    vendor/omni/prebuilt/sounds/extra/alarms/Wisteria.flac:system/media/audio/alarms/Wisteria.flac
+
+# Extra Notifications
+PRODUCT_COPY_FILES += \
+    vendor/omni/prebuilt/sounds/extra/notifications/1up.ogg:system/media/audio/notifications/1up.ogg \
+    vendor/omni/prebuilt/sounds/extra/notifications/3dsms.ogg:system/media/audio/notifications/3dsms.ogg \
+    vendor/omni/prebuilt/sounds/extra/notifications/004.ogg:system/media/audio/notifications/004.ogg \
+    vendor/omni/prebuilt/sounds/extra/notifications/006.ogg:system/media/audio/notifications/006.ogg \
+    vendor/omni/prebuilt/sounds/extra/notifications/008.ogg:system/media/audio/notifications/008.ogg \
+    vendor/omni/prebuilt/sounds/extra/notifications/MarioDie.ogg:system/media/audio/notifications/MarioDie.ogg \
+    vendor/omni/prebuilt/sounds/extra/notifications/MarioJump.ogg:system/media/audio/notifications/MarioJump.ogg \
+    vendor/omni/prebuilt/sounds/extra/notifications/MarioPowerUp.ogg:system/media/audio/notifications/MarioPowerUp.ogg \
+    vendor/omni/prebuilt/sounds/extra/notifications/meow1.ogg:system/media/audio/notifications/meow1.ogg \
+    vendor/omni/prebuilt/sounds/extra/notifications/meow2.ogg:system/media/audio/notifications/meow2.ogg \
+    vendor/omni/prebuilt/sounds/extra/notifications/meow3.ogg:system/media/audio/notifications/meow3.ogg \
+    vendor/omni/prebuilt/sounds/extra/notifications/meow4.ogg:system/media/audio/notifications/meow4.ogg \
+    vendor/omni/prebuilt/sounds/extra/notifications/ZeldaKey.ogg:system/media/audio/notifications/ZeldaKey.ogg
 
 # mkshrc
 PRODUCT_COPY_FILES += \
